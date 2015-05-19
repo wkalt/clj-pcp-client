f2d8a6fdce2dbd6efb3c08c2d84aac357a3ce1a0
Richard Clamp <richardc@unixbeard.net>
2015-05-19 15:10:37 +0100

    lein-release plugin: preparing 0.0.2 release

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(defproject puppetlabs/cthun-client "0.0.2-SNAPSHOT"
+(defproject puppetlabs/cthun-client "0.0.2"
   :description "client library for cthun protocol"
   :url "https://github.com/puppetlabs/clj-cthun-client"
   :license {:name ""
