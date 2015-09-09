156b8d42d8ad8166718444582826b7959e1140f3
Richard Clamp <richardc@unixbeard.net>
2015-09-09 10:33:37 +0100

    lein-release plugin: preparing 0.0.6 release

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(defproject puppetlabs/pcp-client "0.0.6-SNAPSHOT"
+(defproject puppetlabs/pcp-client "0.0.6"
   :description "client library for PCP"
   :url "https://github.com/puppetlabs/clj-pcp-client"
   :license {:name ""
