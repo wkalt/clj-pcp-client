499df84942f5a23c0f7834c6c04b0ce1077f7c99
Richard Clamp <richardc@unixbeard.net>
2015-10-22 11:08:43 +0100

    Prepare for 0.2.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/pcp-client "0.1.1-SNAPSHOT"
+(defproject puppetlabs/pcp-client "0.2.0-SNAPSHOT"
   :description "client library for PCP"
   :url "https://github.com/puppetlabs/clj-pcp-client"
   :license {:name "Apache License, Version 2.0"
