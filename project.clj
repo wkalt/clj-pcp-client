7f1e7ce15b0763bab42de9e0f4053a864a5ed2d3
Jenkins CI <ci@puppetlabs.com>
2016-07-25 11:20:57 -0700

    Version 0.3.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 (def ks-version "1.3.0")
 (def i18n-version "0.4.1")
 
-(defproject puppetlabs/pcp-client "0.3.2"
+(defproject puppetlabs/pcp-client "0.3.3-SNAPSHOT"
   :description "client library for PCP"
   :url "https://github.com/puppetlabs/clj-pcp-client"
   :license {:name "Apache License, Version 2.0"
